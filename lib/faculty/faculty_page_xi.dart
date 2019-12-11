afe6804719cc22ab5150ffabaa07fee4207f5be1
imksav <58361148+imksav@users.noreply.github.com>
2019-12-11 19:21:43 +0545

    Update faculty_page_xi.dart

--- lib/faculty/faculty_page_xi.dart
+++ lib/faculty/faculty_page_xi.dart
@@ -4,6 +4,9 @@ import 'package:simple_app/gradeXI/subject_page_management_xi.dart';
 import 'package:simple_app/gradeXI/subject_page_humanities_xi.dart';
 
 class FacultyPageXI extends StatelessWidget {
+  List subjects = ["Science", "Management", "Humanities"];
+  List publication = ["Asmita Publication", "Heritage Publication", "Buddha Publication"];
+
   final String grade;
 
   FacultyPageXI(this.grade);
@@ -14,7 +17,95 @@ class FacultyPageXI extends StatelessWidget {
       appBar: new AppBar(
         title: new Text(grade),
       ),
+      body: ListView.builder(
+        itemCount: 3,
+        shrinkWrap: true,
+        itemBuilder: (BuildContext context, int index) => Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+          child: Card(
+            elevation: 5.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0.0),
+            ),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        width: 60.0,
+                        height: 60.0,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.green,
+                          foregroundColor: Colors.green,
+                          backgroundImage: NetworkImage("https://freepngimg.com/thumb/science/31122-4-science-file-thumb.png"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                        height: 5.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(subjects[index],
+                            style: TextStyle(
+                              color: Colors.purple,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            ),
+                            Text(publication[index],
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            ),
+                          ],
+                        ),
+                    ]
+                  ),
+                  Container(
+                     alignment: Alignment.center,
+                     padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                     child: FlatButton(
+                       onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).push(
+                        new MaterialPageRoute(
+                        builder: (BuildContext context) => new SubjectPageScienceXI(grade),
+                        ),
+                      );
+                    },
+                       color: Colors.red[200],
+                       shape: RoundedRectangleBorder(
+                         borderRadius: BorderRadius.circular(20.0),
+                       ),
+                       child: Text("Click Here"),
+                     ),
+                  ),
 
+    
+
+                ]
+              ),
+            ), 
+          ),
+        ),
+      ),
+     );
+  }
+}
+
+      
+/*
         body: Center(
         child: ListView(
   padding: const EdgeInsets.all(8),
@@ -89,6 +180,4 @@ class FacultyPageXI extends StatelessWidget {
     ]
     ),
     ),
-    );
-  }
-}
+    */
